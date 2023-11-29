97d96dd8257c22412fca65ef4ffbe554a0a1efb7
MoisesFrancoG <moisesfrancogtz@gmail.com>
2023-11-28 19:00:42 -0600

    Se agrega el proyecto

---
+++ js/app.js
@@ -0,0 +1,9 @@
+//document.write("<center><h1>Hola mundo</h1></center>")
+//alert("Bienvenido")
+//["Bienvendo a","UP"].forEach(alert)
+
+/*let num1 = 200
+let num2 = 300
+let reultado = num1 + num2
+
+console.log("El resultado es: ",reultado)*/
